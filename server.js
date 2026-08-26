aa2e8313b25071b7026cc2c75f03a25ac4947715
Dev Kumar <sdev7274@gmail.com>
2026-08-27 01:21:16 +0530

    server updated

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/api/jobs",(req,res)=>{
     const jobs=[
         {
             id:1,
-            title:"frontend Developer",
+            title:"Frontend Developer",
             company:"Tech solution",
             location:"Delhi",
             type:"Full time",
@@ -41,6 +41,36 @@ app.get("/api/jobs",(req,res)=>{
     ];
     res.json(jobs);
 });
+app.get("/api/jobs/:id",(req,res)=>{
+    const jobs=[
+        {   id:1,
+            title:"Frontend Developer",
+            company:"Tech solution",
+            location:"Delhi",
+            type:"Full time",
+            salary:"5-8LPA",
+            skills:["HTML,CSS,JAVASCRIPT"]
+        },
+        {
+            id:3,
+            title:"Full Stack Developer",
+            company:"Webworks",
+            location:"Noida",
+            type:"Full time",
+            salary:"7-10LPA",
+            skills:["HTML","CSS","JAVASCRIPT","Node.js","Express","MongoDB"]
+        }
+    ];
+    const jobId=Number(req.params.id);
+    const job=jobs.find(job=>job.id===jobId);
+    if(!job){
+        return res.status(404).json({
+            message:"Jb not found"
+        });
+    }
+    res.json(job);
+});
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000.")
 });
