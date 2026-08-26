6b7951764b1d7fcfc4a9341c96de76ca5fecb7e3
Dev Kumar <sdev7274@gmail.com>
2026-08-26 23:39:44 +0530

    add function in it

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,1 +1,15 @@
 console.log("Job Portal Javascript is connected.");
+
+async function getjobs() {
+  try{
+    const response=await fetch("/api/jobs");
+
+    const jobs=await response.json();
+
+    console.log(jobs);
+  }
+  catch(error){
+    console.log("Errorfetching jobs:",error);
+  }  
+}
+getjobs();
